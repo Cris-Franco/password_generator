4de53f194c7d88bb0bfb99bad6510a0047121f03
Cris Franco <cris.franco219@gmail.com>
2020-11-14 20:45:46 -0500

    added confirm booleans for user

--- script.js
+++ script.js
@@ -16,7 +16,31 @@ function generatePassword() {
 
 //Prompt for password length
 var passwordLengthInput = prompt("How many characters?");
+//Length conditional
 if (Number(passwordLengthInput < 8) | Number(passwordLengthInput > 128) | isNaN(passwordLengthInput)) {
     alert("Please enter a number between 8 and 128!");
     return;
 } 
+//Special characters boolean
+var specialCharactersInput = confirm("Do you want special characters?");
+if (specialCharactersInput) {
+    passwordCharset += specialCharacters;
+}
+
+//Uppercase letters boolean
+var uppercaseCharactersInput = confirm("Do you want uppercase letters?");
+if (uppercaseCharactersInput) {
+    passwordCharset += uppercaseCharacters;
+}
+
+//Lowercase letters boolean
+var lowercaseCharactersInput = confirm("Do you want lowercase letters?");
+if (lowercaseCharactersInput) {
+    passwordCharset += lowercaseCharacters;
+}
+
+//Numerical Characters boolean
+var numericalCharactersInput = confirm("Do you want numbers?");
+if (numericalCharactersInput) {
+    passwordCharset += numericalCharacters;
+}
