d7b5e29300e1ae8e2427b868e4888f9a1e6cee62
Cris Franco <cris.franco219@gmail.com>
2020-11-14 20:41:29 -0500

    declared variables and characters used

---
+++ script.js
@@ -0,0 +1,9 @@
+//Declare variables
+var passwordTextarea = document.querySelector("#password");
+var generateButton = document.querySelector("#generate");
+
+//Declare characters used
+const specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+const uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
+const lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+const numericalCharacters = "0123456789";
